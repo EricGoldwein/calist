158f683da5dc691255bbb6f7fa5fe9c0927f49d9
EricGoldwein <eric.goldwein@gmail.com>
2025-03-20 14:34:03 -0400

    Add Flask app for PythonAnywhere deployment

---
+++ flask_app.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('.', path)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
